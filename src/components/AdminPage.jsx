b7a0b1df9f200b34a44dcc2960dfcb15890f85be
Kayleigh <kaylvigh@gmail.com>
2023-12-21 04:35:17 -0330

    Edited the admin page, this needs the updated back end to work though

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,79 +1,127 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../AdminCSS.css';
+import AirportSelect from './AirportSelect';
 
 const AdminPage = () => {
-    const [selectedAirport, setSelectedAirport] = useState('');
-    const [airline, setAirline] = useState('');
-    const [aircraft, setAircraft] = useState('');
-    const [gate, setGate] = useState('');
+    const [airlines, setAirlines] = useState([]);
+    const [aircraft, setAircraft] = useState([]);
+    const [gate, setGate] = useState([]);
     const [flightType, setFlightType] = useState('');
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Placeholder for submission logic
-        /*
-        fetch('http://localhost:8080/flights', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ selectedAirport, airline, aircraft, gate, flightType }),
-        })
-        .then(response => response.json())
-        .then(data => {
-          // Handle response
-        })
-        .catch(error => {
-          // Handle error
-        });
-        */
+    const [selectedAirport, setSelectedAirport] = useState('');
+    const [selectedAirline, setSelectedAirline] = useState('');
+    const [selectedAircraft, setSelectedAircraft] = useState('');
+    const [selectedGate, setSelectedGate] = useState('');
+    const handleAirportSelect = (airportId) => {
+        setSelectedAirport(airportId);
     };
 
+    // Fetch Airlines - - - -
+    useEffect(() => {
+        fetch('http://localhost:8080/airline')
+            .then(response => {
+                if (!response.ok) {
+                    return response.text().then(text => { throw new Error(text || 'Server responded with an error') });
+                }
+                return response.json();
+            })
+            .then(data => {
+                setAirlines(data);
+            })
+            .catch(error => {
+                console.error('Error fetching airlines:', error);
+            });
+    }, []);
+
+    // Fetch Gates - - - -
+    useEffect(() => {
+        if (selectedAirport) {
+            fetch(`http://localhost:8080/gate/byAirport/${selectedAirport}`)
+                .then(response => response.json())
+                .then(data => {
+                    setGate(data);
+                })
+                .catch(error => {
+                    console.error('Error fetching airports:', error);
+                });
+        }
+    }, [selectedAirport]);
+
+    // Fetch Aircrafts - - - -
+    useEffect(() => {
+        if (selectedAirline) {
+            fetch(`http://localhost:8080/aircraft/byAirline/${selectedAirline}`)
+                .then(response => response.json())
+                .then(data => {
+                    setAircraft(data);
+                })
+                .catch(error => {
+                    console.error('Error fetching aircrafts:', error);
+                });
+        }
+    }, [selectedAirline]);
+
+    // Submission - - - -
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Submission logic here
+    };
+
+// ADMIN PAGE - - - - - - - - - - - - - -
     return (
         <div className="adminPage">
             <h1 className="headerAddFlight">ADD A FLIGHT</h1>
             <div className="selectAirport">
                 <div className="selectAirportContainer">
                     <h2 className="selectAirportLabel">SELECT AN AIRPORT</h2>
+                    <AirportSelect onSelectAirport={handleAirportSelect} />
+                </div>
+            </div>
+                <div className="inputGroup">
+                   <div className="airlineInput">
+                       <h2>AIRLINE</h2>
+                       <select
+                           value={selectedAirline}
+                           onChange={(e) => setSelectedAirline(e.target.value)}
+                           className="airlineSelect"
+                           disabled={!selectedAirport}
+                       >
+                           <option value="">Select an airline</option>
+                           {airlines.map((airline) => (
+                               <option key={airline.id} value={airline.id}>{airline.name}</option>
+                           ))}
+                       </select>
+                   </div>
+                <div className="aircraftInput">
+                    <h2>AIRCRAFT</h2>
                     <select
-                        value={selectedAirport}
-                        onChange={(e) => setSelectedAirport(e.target.value)}
-                        className="airportSelect"
+                        value={selectedAircraft}
+                        onChange={(e) => setSelectedAircraft(e.target.value)}
+                        className="aircraftSelect"
+                        disabled={!selectedAirline || !aircraft.length}
                     >
-                        <option value="">Select an airport</option>
-                        {/* Map through airports here */}
+                        <option value="">Select an aircraft</option>
+                        {aircraft.map((ac) => (
+                            <option key={ac.id} value={ac.id}>{ac.name}</option>
+                        ))}
+                    </select>
+                </div>
+
+                <div className="gateInput">
+                    <h2>GATE</h2>
+                    <select
+                        value={selectedGate}
+                        onChange={(e) => setSelectedGate(e.target.value)}
+                        className="gateSelect"
+                        disabled={!selectedAirport}
+                    >
+                        <option value="">Select a gate</option>
+                        {gate.map((gate) => (
+                            <option key={gate.id} value={gate.id}>{gate.name}</option>
+                        ))}
                     </select>
                 </div>
             </div>
-            <div className="inputGroup">
-                <div className="airlineInput">
-                    <h2>AIRLINE</h2>
-                    <input
-                        type="text"
-                        value={airline}
-                        onChange={(e) => setAirline(e.target.value)}
-                        className="airlineInput"
-                    />
-                </div>
-                <div className="aircraftInput">
-                    <h2>AIRCRAFT</h2>
-                    <input
-                        type="text"
-                        value={aircraft}
-                        onChange={(e) => setAircraft(e.target.value)}
-                        className="aircraftInput"
-                    />
-                </div>
-                <div className="gateInput">
-                    <h2>GATE</h2>
-                    <input
-                        type="text"
-                        value={gate}
-                        onChange={(e) => setGate(e.target.value)}
-                        className="gateInput"
-                    />
-                </div>
-            </div>
+
             <div className="flightTypeSelect">
                 <h2>DEPARTING/ARRIVING</h2>
                 <select
@@ -82,10 +130,11 @@ const AdminPage = () => {
                     className="typeSelect"
                 >
                     <option value="">Select Type</option>
-                    <option value="departing">Departing</option>
-                    <option value="arriving">Arriving</option>
+                    <option value="Departing">Departing</option>
+                    <option value="Arriving">Arriving</option>
                 </select>
             </div>
+
             <button onClick={handleSubmit} className="submitFlight">Submit</button>
         </div>
     );
